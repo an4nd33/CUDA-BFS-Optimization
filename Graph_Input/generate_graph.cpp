53dab4d40d7e88a505aae34f83c850dffdbf99ee
anand-282 <apmltr.282@gmail.com>
2026-04-01 10:14:07 +0530

    Added large graph generator and generated graph dataset

---
+++ Graph_Input/generate_graph.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int V = 10000;   // Number of vertices
+    int E = 50000;   // Number of edges
+
+    ofstream file("graph.txt");
+    file << V << " " << E << endl;
+
+    srand(time(0));
+
+    for (int i = 0; i < E; i++) {
+        int u = rand() % V;
+        int v = rand() % V;
+
+        if (u != v)
+            file << u << " " << v << endl;
+        else
+            i--; // avoid self-loop
+    }
+
+    file.close();
+
+    cout << "Graph generated successfully!" << endl;
+    cout << "Vertices: " << V << endl;
+    cout << "Edges: " << E << endl;
+
+    return 0;
+}
